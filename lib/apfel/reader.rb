f58fbcfe22f028b5dffde90254f3a7db46f514ba
Simon Prévost <sprevost@mirego.com>
2014-07-18 10:47:25 -0400

    Convert utf16 to utf8 file on read

--- lib/apfel/reader.rb
+++ lib/apfel/reader.rb
@@ -6,10 +6,18 @@ module Apfel
     def self.read(file)
       File.open(file, 'r') do |f|
         content = f.read.force_encoding('UTF-8')
+
+        # File is utf16, let’s fix this.
+        if content.chars.first == "\xFF".force_encoding('UTF-8')
+          ec = Encoding::Converter.new('UTF-16LE', 'UTF-8')
+          content = ec.convert content
+        end
+
         # remove the BOM that can be found at char 0 in UTF8 strings files
         if content.chars.first == "\xEF\xBB\xBF".force_encoding('UTF-8')
           content.slice!(0)
         end
+
         content.each_line.inject([]) do |content_array, line|
           line.gsub!("\n","")
           content_array.push(line)
